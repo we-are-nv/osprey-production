845db66ece96a88b87ac5f5f6ba2ff7a59bde338
yenmangu <robert.shelford@googlemail.com>
2023-04-02 13:50:20 +0100

    quick save, committed from script, deploying to server

--- build.sh
+++ build.sh
@@ -7,12 +7,9 @@ ssh-add ~/.ssh/github
 echo "Removing old containers"
 docker rm $(docker ps)
 
-echo "Removing database"
-sudo rm -rf src/sql/mysql
-
 echo "Removing old images"
 sudo docker image rm $(docker image ls -a)
 
 echo "Building application"
-docker-compose -f docker-compose-dev.yml build
+docker-compose -f docker-compose-dev-2.yml build
 
