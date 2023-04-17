50ca7e5b92d2512ee74fbda060ed0af77b95507b
Jack Cooper <jack@acooper.net>
2023-04-17 13:22:41 +0100

    initialbackendpush

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,54 +13,7 @@ router.use(function timeLog(req, res, next) {
 
 // DB Connection Test
 
-router.get('/db-connector-test', async (req, res) => {
-	try {
-		var data = {};
-		const message = 'Results from camInfo are: ';
-		let results = await dbQuery.genericQuery('SELECT * FROM `info`');
 
-		data = {
-			message: message,
-			results: results
-		};
-		res.send(data);
-	} catch (e) {
-		console.error(e);
-	}
-});
-
-// Home & Static Pages
-
-router.get('/', (req, res) => {
-	// res.send(process.env);
-	res.render('index');
-});
-
-router.get('/new-site', (req, res) => {
-	res.render('index3');
-});
-
-// Other Static
-
-router.get('/about', breadcrumbs.Middleware(), (req, res) => {
-	res.render('about', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/contact-us', breadcrumbs.Middleware(), (req, res) => {
-	res.render('contact', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/products', breadcrumbs.Middleware(), (req, res) => {
-	res.render('product-collection', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/services', breadcrumbs.Middleware(), (req, res) => {
-	res.render('services-collection', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/resources', breadcrumbs.Middleware(), (req, res) => {
-	res.render('resources', { breadcrumbs: req.breadcrumbs });
-});
 
 router.get('/certifications', breadcrumbs.Middleware(), (req, res) => {
 	res.render('certifications', {
@@ -71,69 +24,13 @@ router.get('/certifications', breadcrumbs.Middleware(), (req, res) => {
 	});
 });
 
-router.get('/news', breadcrumbs.Middleware(), (req, res) => {
-	res.render('news', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/privacy', breadcrumbs.Middleware(), (req, res) => {
-	res.render('privacy', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/terms-and-conditions', breadcrumbs.Middleware(), (req, res) => {
-	res.render('terms', { breadcrumbs: req.breadcrumbs });
-});
 
 // Services Routes
 
-router.get('/services/system-design', breadcrumbs.Middleware(), (req, res) => {
-	res.render('system-design-build', { breadcrumbs: req.breadcrumbs });
-});
 
-router.get('/services/installations', breadcrumbs.Middleware(), (req, res) => {
-	res.render('installations', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/services/service-support', breadcrumbs.Middleware(), (req, res) => {
-	res.render('service-support', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/services/training', breadcrumbs.Middleware(), (req, res) => {
-	res.render('training', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/services/consultancy', breadcrumbs.Middleware(), (req, res) => {
-	res.render('consultancy', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/services/risk', breadcrumbs.Middleware(), (req, res) => {
-	res.render('risk-assessment', { breadcrumbs: req.breadcrumbs });
-});
 
 // Resources
 
-router.get('/resources', breadcrumbs.Middleware(), (req, res) => {
-	res.render('resources', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/resources/datasheets', breadcrumbs.Middleware(), (req, res) => {
-	res.render('datasheets', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/resources/gallery', breadcrumbs.Middleware(), (req, res) => {
-	res.render('gallery', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/resources/knowledge', breadcrumbs.Middleware(), (req, res) => {
-	res.render('knowledge-centre', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/resources/tools', breadcrumbs.Middleware(), (req, res) => {
-	res.render('tools', { breadcrumbs: req.breadcrumbs });
-});
-
-router.get('/resources/press', breadcrumbs.Middleware(), (req, res) => {
-	res.render('press', { breadcrumbs: req.breadcrumbs });
-});
 
 // Markets
 
@@ -152,9 +49,6 @@ router.get('/markets/marine', breadcrumbs.Middleware(), async (req, res) => {
 	});
 });
 
-router.get('/markets/marine-onshore', breadcrumbs.Middleware(), (req, res) => {
-	res.render('marine-onshore', { breadcrumbs: req.breadcrumbs });
-});
 
 router.get('/markets/marine-offshore', breadcrumbs.Middleware(), (req, res) => {
 	res.render('marine-offshore', {
