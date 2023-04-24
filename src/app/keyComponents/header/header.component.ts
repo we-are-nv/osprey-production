0c0680e40364db163c2dad5c6a70af3410005a41
SesifredoDev <20160643@student.chelmsford.ac.uk>
2023-04-24 16:21:44 +0100

    Working on Nav

--- src/app/keyComponents/header/header.component.ts
+++ src/app/keyComponents/header/header.component.ts
@@ -1,14 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   scrolled = false;
-  productCatagories = ["Example 1", ""] 
-
+  finalNav = {
+    home:{path:"", name:"Home"},
+    contact:{path:"", name:"Contact"},
+    dropDownMenus:[
+      {
+        name:"About Us",
+         headpath:"", 
+         childLinks:[
+          {name:"History", path:""}
+         ]
+      },
+      {
+        name:"Products",
+        headpath:"products", 
+        childLinks:[
+        {name:"example", path:""}
+        ]
+      },
+      {
+        name:"Markets",
+        headpath:"", 
+        childLinks:[
+        {name:"example", path:""}
+        ]
+      },
+      {
+        name:"Services",
+        headpath:"services", 
+        childLinks:[
+        {name:"example", path:""}
+        ]
+      },
+      {
+        name:"Recourses",
+        headpath:"", 
+        childLinks:[
+        {name:"example", path:""}
+        ]
+      },  
+    ]
+    }
+    
+  ngOnInit(): void {
+    console.log(this.finalNav)
+  }
   // On scroll detected, Set the toolbar class
   onWindowScroll(event:any){
     if(window.pageYOffset > 20){
