6cd91f1b15c0ff570d3566f70813953ea70e1d95
Jack Cooper <jc3053765@gmail.com>
2023-04-24 11:59:38 +0100

    commit

---
+++ src/models/product_info.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const productInfo = mongoose.Schema({
+    movement:{type:String,required:true}
+})
+
+/*
+prodict code
+prodict name
+image
+technical_images
+
+
+*/
