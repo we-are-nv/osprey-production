50ab558206f7e8b3a359da4b7481fa463253543a
Jack Cooper <jc3053765@gmail.com>
2023-05-13 14:24:16 +0100

    product_searchinh

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -10,6 +10,7 @@ const camProd = require('../models/product_types/camera_prod.js');
 const housProd = require('../models/product_types/housing_prod.js')
 const diskNVR = require('../models/product_types/disk_nvr_prod.js');
 const nvrProd = require('../models/product_types/nvr_prod.js');
+const { all } = require('../controller/dbConnector.js');
 
 var providers = {
   disk_nvr: diskNVR
@@ -36,7 +37,7 @@ router.use(function timeLog(req, res, next) {
 
 router.get("/product_info", async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
-  if (!req.query.type) {
+  if (!req.query.type && !req.query.documentId) {
     res.json({ error: 'Please Enter a valid product type' });
     return;
   };
@@ -117,7 +118,7 @@ router.get("/product_info", async (req, res) => {
     .populate(popu)
     .then((result) => {
       if (req.query.documentId) {
-        res.json({ product: result[0],page:page, })
+        res.json({ product: result[0] })
       } else {
         res.json({ products: result,   totalPages: Math.ceil(count / limit),
         currentPage: Number(page) })
@@ -126,8 +127,43 @@ router.get("/product_info", async (req, res) => {
     })
 });
 
+/*
+Search For Products
+*/
 
+router.get("/search", async (req, res) => {
+  const { page = 1, limit = 10, searchFor = "product_name", searchQuery = "", extra = false } = req.query;
+  var query = {};
+  if (req.query.type) {
+    var inject = {"productType.modelName":product_types[req.query.type]};
+    query = {...inject,...query}
+  };
+  var inject = {[searchFor]:{$regex:searchQuery,$options: "i"}};
+  query = {...query, ...inject};
+  console.log(query);
+  const count = await allProducts.count(query);
+  var selectOptions = "product_name product_code ";
 
+  if (extra) {
+    selectOptions += "image description"
+  }
+  allProducts.find(query,selectOptions)
+  .limit(limit * 1)
+  .skip((page - 1) * limit)
+  .then((result) => {
+    var newOutput = [];
+    var escapedResult = result;
+    for (idx in escapedResult){
+
+      escapedResult[idx]["product_link"] = `/api/product/product_info?documentId=${escapedResult[idx]._id}`
+
+      newOutput.push(escapedResult[idx])
+    };
+    res.json({ products: newOutput,   totalPages: Math.ceil(count / limit),
+    currentPage: Number(page) })
+  })
+
+})
 
 
 module.exports = router;
