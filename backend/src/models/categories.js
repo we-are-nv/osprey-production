2dd8449654d50aff1249b1c6243c351d39af35e4
wearenv-digital <design@wearenv.co.uk>
2023-06-14 16:07:05 +0100

    models and categories

--- backend/src/models/categories.js
+++ backend/src/models/categories.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
-
+// For prod, change required: true
 
 const categoryInfo = mongoose.Schema({
   name:{type:String,requried:true,unique:true},
-  image:{type:String,required:true},
+  image: { type: String, required: true },
+  info: {
+    heading: { type: String, required: false },
+    sub_heading: { type: String, required: false },
+    banner_image: {type: String, required: false}
+  },
+  subCats: {type: Array, required: false}
 })
 
 
