ae9274e6b2142ca7569a2c666e7fbb50ecaf0921
Jack Cooper <jack@acooper.net>
2023-06-16 15:57:58 +0100

    markets

--- backend/src/routes/information.js
+++ backend/src/routes/information.js
@@ -35,8 +35,8 @@ router.post('/', checkAuth, async (req, res) => {
   var bannerImageURL = `info/${newMarketID}/images/banner`;
   var thumbnailImageURL = `info/${newMarketID}/images/thumb`;
 
-  uploadBaseMarket(bannerImageURL, req.body.banner);
-  uploadBaseMarket(thumbnailImageURL, req.body.thumb);
+ // uploadBaseMarket(bannerImageURL, req.body.banner);
+  //uploadBaseMarket(thumbnailImageURL, req.body.thumb);
   var bonusObj = new Array();
   for (idx in req.body.bonus) {
     const newBonusID = new mongoose.Types.ObjectId();
@@ -46,7 +46,7 @@ router.post('/', checkAuth, async (req, res) => {
         if (element.source) {
           if (Object.keys(element.source).includes('icon')) {
             var iconURL = `info/${newMarketID}/${newBonusID}/images/elements/${element.source.text + element.source.color}`;
-            uploadBaseMarket(iconURL, element.source.icon)
+            //uploadBaseMarket(iconURL, element.source.icon)
             element.source.icon = `/${iconURL}`;
             currentBonus.elements[idx] = element;
           };
@@ -56,7 +56,7 @@ router.post('/', checkAuth, async (req, res) => {
     } else if (currentBonus.files) {
       currentBonus.files.forEach((element2, idx) => {
         var iconURL = `info/${newMarketID}/${newBonusID}/images/files/icons/${element2.name}`;
-        uploadBaseMarket(iconURL, element2.icon)
+        //uploadBaseMarket(iconURL, element2.icon)
         element2.icon = iconURL;
         currentBonus.files[idx] = element2
 
@@ -76,13 +76,26 @@ router.post('/', checkAuth, async (req, res) => {
       ...currentBonus
     });
 
-    newBonus.save();
-  }
+    //newBonus.save();
+  };
+
+  if (req.body.pages.length > 0){
+    for (pIdx in req.body.pages){
+      const foundPage = await informationPage.findOne({_id:req.body.pages[pIdx]});
+      var obj = {
+        name:foundPage._doc.name,
+        id:req.body.pages[pIdx]
+      };
+      req.body.pages[pIdx] = obj;
+    };
+
+  };
 
   const newMarket = new market({
     _id: newMarketID,
     name: req.body.name,
     type:req.body.type,
+    pages:req.body.pages,
     secondry_title: req.body.secondry,
     lower_title: req.body.lower,
     banner_image: `/${bannerImageURL}`,
@@ -90,7 +103,7 @@ router.post('/', checkAuth, async (req, res) => {
     bonus_cards: bonusObj,
   });
 
-  newMarket.save();
+  //newMarket.save();
   res.json({ message: newMarket });
 
 });
@@ -162,8 +175,46 @@ router.get('/', async (req, res) => {
 router.get('/single', async (req, res) => {
   const foundInfos = await information.findOne({_id:req.query.id})
     .populate({ path: 'bonus_cards.id' })
-    .populate({ path: 'pages.id' })
+    var routeKeys = Object.keys(foundInfos._doc);
+    for (routeIdx in routeKeys){
+      if (routeKeys[routeIdx].includes('image')){
+        foundInfos._doc[routeKeys[routeIdx]] = `${process.env.S3_BASE}${foundInfos._doc[routeKeys[routeIdx]]}`
+      };
+    };
+
+    var bonusCards = foundInfos._doc.bonus_cards;
+    for (bonusIdx in bonusCards){
+      var bonusDoc = bonusCards[bonusIdx]._doc.id._doc;
+      for (elementIdx in bonusDoc.elements){
+        bonusDoc.elements[elementIdx].source.icon = `${process.env.S3_BASE}${bonusDoc.elements[elementIdx].source.icon}`
+        console.log(bonusDoc.elements[elementIdx].source.icon)
+      };
+      for (fileIdx in bonusDoc.files){
+        foundInfos._doc.bonus_cards[bonusIdx].id.files[fileIdx].url = `${process.env.S3_BASE}${bonusDoc.files[fileIdx].url}`;
+        foundInfos._doc.bonus_cards[bonusIdx].id.files[fileIdx].icon = `${process.env.S3_BASE}${bonusDoc.files[fileIdx].icon}`;
+      };
+
+    }
+
+
   res.json(foundInfos)
+});
+
+router.get('/page', async (req,res) => {
+  const foundPage = await informationPage.findOne({_id:req.query.id});
+
+  var pageElements = foundPage._doc.elements;
+
+  for (idx in pageElements){
+    if (pageElements[idx].type == "image"){
+
+      pageElements[idx].images.forEach(function(item,pIdx){
+        pageElements[idx].images[pIdx] = `${process.env.S3_BASE}${pageElements[idx].images[pIdx]}`;
+      });
+    };
+  };
+
+  res.json(foundPage);
 });
 
 
