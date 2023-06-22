9012a715abe79452a531dd0150752e5445bbd35f
pchaffeyNV <paolo@wearenv.co.uk>
2023-06-22 08:39:33 +0100

    bullet point stuff done

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
 	);
 	res.setHeader(
 		'Access-Control-Allow-Methods',
-		'GET, POST, PATCH, DELETE, OPTIONS'
+		'GET, POST, PATCH, DELETE, OPTIONS, PUT'
 	);
 	if (req.method === 'OPTIONS') {
 		res.sendStatus(200);
