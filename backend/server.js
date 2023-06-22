156fa6b50a8b074c57545c9feba19e81869f460c
wearenv-digital <design@wearenv.co.uk>
2023-06-22 10:33:29 +0100

    prod Prod

--- backend/server.js
+++ backend/server.js
@@ -66,11 +66,11 @@ app.use((req, res, next) => {
 	}
 	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization, prod'
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization, Prod'
 	);
 	res.setHeader(
 		'Access-Control-Allow-Methods',
-		'GET, POST, PATCH, DELETE, OPTIONS, PUT, OPTIONS'
+		'GET, POST, PATCH, DELETE, OPTIONS, PUT'
 	);
 	if (req.method === 'OPTIONS') {
 		res.sendStatus(200);
