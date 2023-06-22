24a51ad3a04e0874e40578686c13ac9825b2c43d
wearenv-digital <design@wearenv.co.uk>
2023-06-22 10:16:45 +0100

    added options to the header

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
 	);
 	res.setHeader(
 		'Access-Control-Allow-Methods',
-		'GET, POST, PATCH, DELETE, OPTIONS, PUT'
+		'GET, POST, PATCH, DELETE, OPTIONS, PUT, OPTIONS'
 	);
 	if (req.method === 'OPTIONS') {
 		res.sendStatus(200);
