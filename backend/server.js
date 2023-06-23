9adb8d987b5d6127dbe6db2ac5f53ed266fea319
Jack Cooper <jack@acooper.net>
2023-06-23 15:10:10 +0100

    headersCors

--- backend/server.js
+++ backend/server.js
@@ -64,6 +64,7 @@ app.use((req, res, next) => {
 	if (allowedOrigins.includes(origin)) {
 		res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Headers', true);
 	}
 
 	res.setHeader(
