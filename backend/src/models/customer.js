e2cceb03f464262ca60002cfe1fbdb76a66f842e
wearenv-digital <design@wearenv.co.uk>
2023-07-03 15:07:32 +0100

    workspace.settings.json

---
+++ backend/src/models/customer.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const customerSchema = mongoose.Schema({
+	first_name: { type: String, required: true },
+	last_name: { type: String, required: true },
+	email: { type: String, required: true, match: /.+\@.+\..+/ },
+	phone_number: { type: String, required: true },
+	organisation: { type: String, required: false },
+	location: { type: String, required: false },
+	message: { type: String, required: false }
+});
+
+module.exports = mongoose.model('customer', customerSchema);
+
