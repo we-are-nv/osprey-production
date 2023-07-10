31d311d07edcf3884ee7caeb3380f9e65e9579c8
pchaffeyNV <paolo@wearenv.co.uk>
2023-07-10 10:44:02 +0100

    animations and styling

--- src/app/keyComponents/hero/hero.component.ts
+++ src/app/keyComponents/hero/hero.component.ts
@@ -1,31 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
-  templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+	selector: 'app-hero',
+	templateUrl: './hero.component.html',
+	styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent implements OnInit{
-  ngOnInit(){
-    if (typeof window !== 'undefined'){
-      sessionStorage.setItem("navStyle", "standard")
-    };
-  }
-  @Input() mainTitle: string;
-  @Input() secondaryTitle: string;
-  @Input() upperTitle: string;
-  @Input() lowerTitle: string;
-  @Input() background: string;
+export class HeroComponent implements OnInit {
+	ngOnInit() {
+		if (typeof window !== 'undefined') {
+			sessionStorage.setItem('navStyle', 'standard');
+		}
+	}
+	@Input() mainTitle: string;
+	@Input() secondaryTitle: string;
+	@Input() upperTitle: string;
+	@Input() lowerTitle: string;
+	@Input() background: string;
 
-  // button data
-  @Input() isButtons: boolean;
-  @Input() isButton3: boolean;
+	// button data
+	@Input() isButtons: boolean;
+	@Input() isButton3: boolean;
 
-  @Input() button1Data: {text:string, path:string, type:string};
-  @Input() button2Data: {text:string, path:string, type:string};
-  @Input() button3Data: {text:string, path:string, type:string};
+	@Input() button1Data: { text: string; path: string; type: string };
+	@Input() button2Data: { text: string; path: string; type: string };
+	@Input() button3Data: { text: string; path: string; type: string };
 
-  // Styling
-  @Input() centred: boolean;
-
+	// Styling
+	@Input() centred: boolean;
 }
