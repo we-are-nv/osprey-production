eb016fa41ca1180279378a0283596927bf272c18
pchaffeyNV <paolo@wearenv.co.uk>
2023-07-17 11:34:40 +0100

    spec tabs removed

--- src/app/pages/products/product-view/product-view.component.ts
+++ src/app/pages/products/product-view/product-view.component.ts
@@ -1,123 +1,102 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
-  selector: 'app-product-view',
-  templateUrl: './product-view.component.html',
-  styleUrls: ['./product-view.component.scss']
+	selector: 'app-product-view',
+	templateUrl: './product-view.component.html',
+	styleUrls: ['./product-view.component.scss']
 })
-export class ProductViewComponent implements OnInit{
+export class ProductViewComponent implements OnInit {
+	constructor(
+		private _Activatedroute: ActivatedRoute,
+		private productService: ProductService
+	) {}
+	id: any;
+	productSub: Subscription;
+	similarProductsSub: Subscription;
 
+	category = '';
 
-  constructor(private _Activatedroute:ActivatedRoute, private productService: ProductService){}
-  id:any;
-  productSub: Subscription;
-  similarProductsSub: Subscription;
+	product: any = null;
 
-  category = ""
+	history: any;
 
+	additionalInfo: any = [];
+	displayedColumns: string[] = ['detail', 'value'];
 
-  product: any = null;
+	ngOnInit() {
+		this.additionalInfo = [];
 
-  history: any;
+		this._Activatedroute.params.subscribe(params => {
+			// Reads parameters from URL
 
-  additionalInfo: any = []
-  displayedColumns: string[] = ['detail', 'value'];
+			this.id = params['id'];
+			//this.category = params['category'];
 
+			// Get the product from the URL parameters
 
-  ngOnInit() {
-    this.additionalInfo = [];
+			this.productSub = this.productService
+				.getSingleProductUpdateListener()
+				.subscribe(data => {
+					this.product = data;
+					console.log(this.product);
 
-    this._Activatedroute.params.subscribe(params => {
+					this.history = [
+						{ path: '/', friendly: 'Home' },
+						{ path: '/products/landing', friendly: 'Products' },
+						// {path:"/products/"+this.product.category, friendly:this.product.category},
+						{
+							path: '/product/' + this.product.category + '/' + this.product._id,
+							friendly: this.product.product_name
+						}
+					];
 
-      // Reads parameters from URL
+					let tempAdditionalInfo = this.product.additional_information.info;
+					this.informationConverter(tempAdditionalInfo);
 
-      this.id = params['id'];
-      //this.category = params['category'];
+					console.log();
+					// Gets all the similar products
+				});
+			this.productService.getSingleProduct(this.id);
+		});
+	}
 
+	// Converts additional information to readable format
 
+	informationConverter(info: any) {
+		this.additionalInfo = [];
+		for (let k in info) {
+			let v = info[k];
+			for (let item in v) {
+				if (this.specValidation(v[item])) {
+					let itemObj = {
+						name: item,
+						value: v[item].toString()
+					};
 
-      // Get the product from the URL parameters
+					this.additionalInfo.push(itemObj);
+				}
+			}
+		}
+	}
 
+	specValidation(data: any): boolean {
+		if (
+			data !== '' &&
+			data !== null &&
+			data !== undefined &&
+			data !== 'n/a' &&
+			data !== 'na'
+		) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-      this.productSub = this.productService.getSingleProductUpdateListener()
-      .subscribe((data)=>{
-        this.product = data;
-        console.log(this.product)
-        
-
-        this.history = [
-          {path:"/", friendly:"Home"},
-          {path:"/products/landing", friendly:"Products"},
-          // {path:"/products/"+this.product.category, friendly:this.product.category},
-          {path:("/product/" + this.product.category+ '/' + this.product._id), friendly:this.product.product_name}];
-
-        let tempAdditionalInfo = this.product.additional_information.info
-        this.informationConverter(tempAdditionalInfo)
-
-        console.log()
-        // Gets all the similar products
-
-
-        
-
-
-    });
-    this.productService.getSingleProduct(this.id)
-    });
-  }
-
-  // Converts additional information to readable format
-
-  informationConverter(info:any){
-    this.additionalInfo = []
-    for (let k in info) {
-      let v = info[k];
-      let itemsArray= [];
-      for(let item in v){
-
-          if(this.specValidation(v[item])){
-            let itemObj = {
-              name:item,
-              value:v[item].toString(),
-            };
-
-          itemsArray.push(itemObj);
-        }
-
-
-      }
-      let infoObj = {
-        name:k,
-        items:itemsArray
-      };
-      if(itemsArray.length > 0){
-        this.additionalInfo.push(infoObj);
-      }
-
-      console.log(this.additionalInfo);
-    }
-
-  }
-
-  specValidation(data:any) : boolean{
-    if (data !== '' &&
-        data!== null &&
-        data!== undefined &&
-        data !== "n/a" &&
-        data !== "na") {
-          return true;
-        }
-      else{
-        return false;
-      }
-  }
-
-
-  checking(data:any){
-    console.log(data)
-  }
-
+	checking(data: any) {
+		console.log(data);
+	}
 }
