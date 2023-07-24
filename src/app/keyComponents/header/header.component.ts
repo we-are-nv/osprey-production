f5a5178cde1959ac4605a7f7f314a222f4ee06eb
Jack Cooper <jack@acooper.net>
2023-07-24 17:02:53 +0100

    Searchfix

--- src/app/keyComponents/header/header.component.ts
+++ src/app/keyComponents/header/header.component.ts
@@ -1,10 +1,10 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-import { MatMenuTrigger } from '@angular/material/menu';
-import { Router } from '@angular/router';
-import { InfoPageService } from 'src/app/services/info-page.service';
-import { ProductService } from 'src/app/services/product.service';
-import { DatabaseService } from 'src/app/services/database.service';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
+import {MatMenuTrigger} from '@angular/material/menu';
+import {Router} from '@angular/router';
+import {InfoPageService} from 'src/app/services/info-page.service';
+import {ProductService} from 'src/app/services/product.service';
+import {DatabaseService} from 'src/app/services/database.service';
 
 @Component({
 	selector: 'app-header',
@@ -12,14 +12,14 @@ import { DatabaseService } from 'src/app/services/database.service';
 	styleUrls: ['./header.component.scss'],
 	animations: [
 		trigger('inputWiden', [
-			state('normal', style({ width: '1vw', opacity: '0' })), // Initial state
-			state('widen', style({ width: '50vw', opacity: '1' })), // Widened state
+			state('normal', style({width: '1vw', opacity: '0'})), // Initial state
+			state('widen', style({width: '50vw', opacity: '1'})), // Widened state
 			transition('normal => widen', animate('2000ms ease-in')), // Transition time and easing function
 			transition('widen => normal', animate('2000ms ease-out')) // Transition time and easing function
 		]),
 		trigger('navShrink', [
-			state('normal', style({ width: 'auto', opacity: '1' })), // Initial state
-			state('widen', style({ width: '0%', opacity: '0' })), // Widened state
+			state('normal', style({width: 'auto', opacity: '1'})), // Initial state
+			state('widen', style({width: '0%', opacity: '0'})), // Widened state
 			transition('normal => widen', animate('0500ms ease-in')), // Transition time and easing function
 			transition('widen => normal', animate('3500ms ease-out')) // Transition time and easing function
 		])
@@ -39,6 +39,7 @@ export class HeaderComponent {
 	navStyle = 'standard';
 
 	isSearching = false;
+	searchArray: any[] = [];
 	searchData: any;
 
 	@Input() finalNav: any;
@@ -69,15 +70,18 @@ export class HeaderComponent {
 	}
 
 	onEnter(event: any) {
+		console.log('ENTER');
 		console.log(event);
 	}
 
 	onSearchChange(searchValue: any): void {
+		console.log('SEARCHCAH');
 		console.log(searchValue.target.value);
 		this.databaseService
 			.searchAll(searchValue.target.value)
 			.subscribe((data: any) => {
 				this.searchData = data.results;
+				this.searchArray = data.results.product;
 				console.log(this.searchData.product);
 				console.log(this.searchData);
 			});
