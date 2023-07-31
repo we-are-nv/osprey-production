a08015a63c117539cd4017e3a01777192046f6bf
Jack Cooper <jack@acooper.net>
2023-07-31 16:22:39 +0100

    fiendlyName

--- backend/src/models/product_varients.js
+++ backend/src/models/product_varients.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 
 const productVarient = mongoose.Schema({
-  data: { type: Array, required: true }
+  data: { type: Array, required: true },
+  friendly: { type: String, required: true },
 });
 
 module.exports = mongoose.model('product_varient', productVarient);
