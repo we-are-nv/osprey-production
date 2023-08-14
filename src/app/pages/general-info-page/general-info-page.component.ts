1c824c817a36c5f00d38c4f2bbb6e77a5732aadc
Jack Cooper <jack@acooper.net>
2023-08-14 15:39:18 +0100

    test

--- src/app/pages/general-info-page/general-info-page.component.ts
+++ src/app/pages/general-info-page/general-info-page.component.ts
@@ -1,7 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { InfoPageService } from 'src/app/services/info-page.service';
-
+import { environment } from 'src/environments/environment';
 @Component({
 	selector: 'app-general-info-page',
 	templateUrl: './general-info-page.component.html',
@@ -11,9 +12,10 @@ export class GeneralInfoPageComponent implements OnInit {
 	constructor(
 		private _Activatedroute: ActivatedRoute,
 		private infoService: InfoPageService,
-		private router: Router
+		private router: Router,
+    private http: HttpClient
 	) {}
-
+  private API_URL = environment.API_URL;
 	@Input() isHero: boolean = true;
 	@Input() type: string;
 	@Input() page: string;
@@ -40,42 +42,76 @@ export class GeneralInfoPageComponent implements OnInit {
 		this.subPages = [];
 
 
-
-		this._Activatedroute.params.subscribe(params => {
-			if(this.type == undefined){
+    this._Activatedroute.params.subscribe(params => {
+      let renderInfo: any = this._Activatedroute.snapshot.data;
+      let renderType = renderInfo.type;
+      if(this.type == undefined){
 				this.pageType = params['type'];
 				this.pageId = params['id'];
 			}else{
 				this.pageType = this.type
 				this.pageId = this.page;
 			}
-			
 
-			let siblingSub = this.infoService.getThumbnails(this.pageType).subscribe((data: any) => {
-				this.siblingPages = data;
+      if (renderType == "id") {
 
-				siblingSub.unsubscribe();
-			});
+        let siblingSub = this.infoService.getThumbnails(this.pageType).subscribe((data: any) => {
+          this.siblingPages = data;
 
-			// Get Main Page
-			let pageSub = this.infoService.getMainUpdateListener().subscribe(data => {
-				this.pageData = data;
-				this.subPages = data.pages;
-				this.bonusCards = data.bonus_cards;
-				this.activeSubPageId = this.subPages[0].id;
-        console.log(data)
-        if (data.suggestProducts){
-          this.suggestedProducts = `info-${data._id}`
-        }
-				this.router.navigate([
-					`/info-page/${this.pageType}/${this.pageId}/${this.activeSubPageId}`
-				]);
+          siblingSub.unsubscribe();
+        });
 
-				pageSub.unsubscribe();
-			});
+        // Get Main Page
+        let pageSub = this.infoService.getMainUpdateListener().subscribe(data => {
+          this.pageData = data;
+          this.subPages = data.pages;
+          this.bonusCards = data.bonus_cards;
+          this.activeSubPageId = this.subPages[0].id;
+          console.log(data)
+          if (data.suggestProducts) {
+            this.suggestedProducts = `info-${data._id}`
+          }
+          this.router.navigate([
+            `/info-page/${this.pageType}/${this.pageId}/${this.activeSubPageId}`
+          ]);
 
-			this.infoService.getMainPage(this.pageId);
-		});
+          pageSub.unsubscribe();
+        });
+
+        this.infoService.getMainPage(this.pageId);
+      } else {
+        this.http
+        .get<any>(this.API_URL + '/info/convert-route?name=' + params['name'] + '&type='+ params['type'])
+        .subscribe(response => {
+          let siblingSub = this.infoService.getThumbnails(this.pageType).subscribe((data: any) => {
+            this.siblingPages = data;
+
+            siblingSub.unsubscribe();
+          });
+
+          // Get Main Page
+          let pageSub = this.infoService.getMainUpdateListener().subscribe(data => {
+            this.pageData = data;
+            this.subPages = data.pages;
+            this.bonusCards = data.bonus_cards;
+            this.activeSubPageId = this.subPages[0].id;
+            console.log(data)
+            if (data.suggestProducts) {
+              this.suggestedProducts = `info-${data._id}`
+            }
+            this.router.navigate([
+              `/info/${this.pageType}/${this.pageId}/${response.activeSub}`
+            ]);
+
+            pageSub.unsubscribe();
+          });
+
+          this.infoService.getMainPage(response._id);
+        })
+      }
+
+    });
+
 	}
 
 	selectPage(id: string) {
